bf57bad053d62e09f2023a5f39b6bda568cea535
Aththanayaka A.M.U.S <100036021+UpadhyaSilumini99@users.noreply.github.com>
2023-06-20 00:10:19 +0530

    front end and backend upload

---
+++ frontend/src/component/Welcome.js
@@ -0,0 +1,39 @@
+import { Grid, Typography } from "@material-ui/core";
+
+const Welcome = (props) => {
+  return (
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ padding: "30px", minHeight: "93vh", backgroundColor: "#0E2954", backgroundImage: `url("../images/sljobhub.png)` }}
+    >
+      <Grid item>
+        <Typography variant="h2" style={{color: "#ffffff", fontFamily:"Gabriola"}}>Welcome to SLJobHub</Typography>
+
+
+      </Grid>
+    </Grid>
+  );
+};
+
+export const ErrorPage = (props) => {
+  return (
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ padding: "30px", minHeight: "93vh" }}
+    >
+      <Grid item>
+        <Typography variant="h2">Error 404</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Welcome;
